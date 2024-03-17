e973d1558323f4b34e62c9d8dab85fa78efca5ad
Rohan <rohanrpawar02@gmail.com>
2024-03-17 18:02:31 +0530

    added the user Registration Functionality

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(express.static('public'))
 const fs = require('fs');
 const uploadPath = 'uploads/';
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 // Configure Cloudinary
 cloudinary.config({
@@ -48,9 +50,61 @@ const mediaSchema = new mongoose.Schema({
   // Create a Mongoose model
   const Media = mongoose.model('Media', mediaSchema);
 
+
+// Define the User Schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  // Basic user details
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+   profilePicture: {
+    type: String, // Store the URL of the profile picture
+    default: 'default_profile_picture_url.jpg' // You can set a default image URL if none is provided
+  },
+  // Posts made by the user
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Media'
+  }]
+});
+
+// Define and export the User model
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
   
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
   // Define a route for file upload
-  app.post('/upload', upload.single('file'), async (req, res) => {
+  app.post('/upload',authenticateToken, upload.single('file'), async (req, res) => {
     try {
       // Upload file to Cloudinary
       const result = await cloudinary.uploader.upload(req.file.path,{folder: "memories"});
@@ -82,7 +136,7 @@ app.get('/getposts',(req,res)=>{
     })
   
 
-
+//Update Like count 
 app.patch('/getposts/:id',(req,res)=>{
     const likes = req.body.likes;
     
@@ -110,6 +164,79 @@ app.delete('/delete/:id', async (req, res) => {
 });
 
 
+app.get('/getusers',(req,res)=>{
+  User.find()
+  .then(user =>res.json(user))
+  .catch(err =>res.status(400).json('error: '+err))
+})
+
+//register new user
+app.post("/newuser",upload.single('file'),async (req,res)=>{
+  try {
+
+    const { username, email, password, firstname,lastname } = req.body;
+    const existingEmail = await User.findOne({ email });
+    const existingUsername = await User.findOne({ username });
+    // Upload file to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path,{folder: "memories"});
+
+    if(existingEmail)
+    {
+      return res.status(400).json({ message: 'User with the same email already exists' });
+    }
+    if(existingUsername)
+    {
+      return res.status(400).json({ message: 'User with the same Username already exists' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10); 
+    const user  = new User({
+      username, email, password:hashedPassword, firstname,lastname,profilePicture:result.secure_url 
+    });
+  
+    const userId = await user.save();
+    clearUploadsFolder();
+    const token = jwt.sign({ userId: userId._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    if(user)
+    {
+      return res.status(201).json({ message: 'User Created Profile Successfully with Userid '+userId._id });
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: `An error occurred ${error}` });
+  }
+
+})
+
+// POST route for user login
+app.post('/userlogin', async (req, res) => {
+  try {
+    const { usernameOrEmail, password } = req.body;
+
+    // Check if a user with the provided username or email exists
+    const user = await User.findOne({ $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }] });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Verify password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) { 
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+    
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 
 // Helper function to extract public ID from Cloudinary URL
